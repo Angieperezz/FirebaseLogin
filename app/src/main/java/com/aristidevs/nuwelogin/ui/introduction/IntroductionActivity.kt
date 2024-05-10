a016ba49c5a13ee0b3670588956689f34c18abc7
Angelinperezz <angelin.perez@sitiouno.com>
2024-05-09 23:37:53 -0400

    Se implementa carrusel que por ahora funciona es con texto

--- app/src/main/java/com/aristidevs/nuwelogin/ui/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/aristidevs/nuwelogin/ui/introduction/IntroductionActivity.kt
@@ -1,5 +1,6 @@
 package com.aristidevs.nuwelogin.ui.introduction
 
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
@@ -9,11 +10,18 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.aristidevs.nuwelogin.R
 import com.aristidevs.nuwelogin.databinding.ActivityIntroductionBinding
+import com.aristidevs.nuwelogin.domain.CarouselRVAdapter
 import com.aristidevs.nuwelogin.ui.login.LoginActivity
 import com.aristidevs.nuwelogin.ui.signin.SignInActivity
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
 
 
 @AndroidEntryPoint
@@ -24,12 +32,12 @@ class IntroductionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Nuwe)
-        setTheme(com.aristidevs.nuwelogin.R.style.Theme_Nuwe)
         super.onCreate(savedInstanceState)
         binding = ActivityIntroductionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val texto = "Compra activos tokenizados desde \n cualquier parte del mundo"
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
 
         val spannableString = SpannableString(texto)
         spannableString.setSpan(
@@ -48,6 +56,31 @@ class IntroductionActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(com.aristidevs.nuwelogin.R.id.appCompatTextView)
         textView.text = spannableString
         initUI()
+
+        viewPager.apply{
+            clipChildren = false
+            clipToPadding = false
+            offscreenPageLimit = 3
+            (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        }
+
+        val demoData = arrayListOf(
+            "img1",
+            "img2",
+            "img3",
+            "img4",
+            "img5"
+        )
+
+        viewPager.adapter = CarouselRVAdapter(demoData)
+
+        val compositePageTransformer = CompositePageTransformer()
+        compositePageTransformer.addTransformer(MarginPageTransformer((40 * Resources.getSystem().displayMetrics.density).toInt()))
+        compositePageTransformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = (0.80f + r * 0.20f)
+        }
+        viewPager.setPageTransformer(compositePageTransformer)
     }
 
     private fun initUI() {

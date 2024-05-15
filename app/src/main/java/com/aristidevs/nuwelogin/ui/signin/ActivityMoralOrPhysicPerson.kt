99c4bce0aba53feeaeb272c7b24f576fa08c8f24
Angelinperezz <angelin.perez@sitiouno.com>
2024-05-15 13:46:32 -0400

    Se agrega activity con los cardViews
    Falta corregir un monton

--- app/src/main/java/com/aristidevs/nuwelogin/ui/signin/ActivityMoralOrPhysicPerson.kt
+++ app/src/main/java/com/aristidevs/nuwelogin/ui/signin/ActivityMoralOrPhysicPerson.kt
@@ -1,26 +1,22 @@
 package com.aristidevs.nuwelogin.ui.signin
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.aristidevs.nuwelogin.R
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
 
-class SignInActivityEnterData : AppCompatActivity(){
+class ActivityMoralOrPhysicPerson: AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_know_you)
+        setContentView(R.layout.choose_who_you_are_activity)
 
         val btnNext = findViewById<MaterialButton>(R.id.btnNext)
         btnNext.setOnClickListener {
-            val intent = Intent(this, ActivityMoralOrPhysicPerson::class.java)
-            startActivity(intent)
-         //startActivity(VerificationActivity.create(this))
+            //startActivity(VerificationActivity.create(this))
         }
 
 
     }
-
 }

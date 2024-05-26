5fd96a8a81fdd2b466cffe01b9240a982e68b728
Angelinperezz <angelin.perez@sitiouno.com>
2024-05-26 16:00:42 -0400

    Avances del dashboard, se hace la conexión entre vistas y se agrega navigation

---
+++ app/src/main/java/com/aristidevs/nuwelogin/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,32 @@
+package com.aristidevs.nuwelogin.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aristidevs.nuwelogin.R
+import com.aristidevs.nuwelogin.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/aristidevs/nuwelogin/ui/signin/SignInResultValidationCode.kt
+++ app/src/main/java/com/aristidevs/nuwelogin/ui/signin/SignInResultValidationCode.kt
@@ -1,19 +1,23 @@
 package com.aristidevs.nuwelogin.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.aristidevs.nuwelogin.R
+import com.aristidevs.nuwelogin.ui.dashboard.DashboardActivity
 import com.aristidevs.nuwelogin.ui.signin.VerifyUserEmail.Companion.codeIntroducedEmail
 import com.aristidevs.nuwelogin.ui.signin.VerifyUserEmail.Companion.isUserEmailCode
 import com.aristidevs.nuwelogin.ui.signin.VerifyUserPhone.Companion.codeIntroduced
 import com.aristidevs.nuwelogin.ui.signin.VerifyUserPhone.Companion.isUserPhoneCode
+import com.google.android.material.button.MaterialButton
 
 class SignInResultValidationCode: AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var textView: TextView
+    private lateinit var btnNext: MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,7 @@ class SignInResultValidationCode: AppCompatActivity() {
 
         imageView = findViewById(R.id.imageCheckSuccessOrFail)
         textView = findViewById(R.id.tvVerification)
+        btnNext = findViewById(R.id.btnNext)
 
         initUI()
 
@@ -30,6 +35,10 @@ class SignInResultValidationCode: AppCompatActivity() {
         if(codeIntroduced == "1234" && isUserPhoneCode){
             imageView.setImageResource(R.drawable.success_check)
             textView.text = ("Hemos confirmado correctamente tu \nnúmero de teléfono")
+            btnNext.setOnClickListener{
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            }
 
         } else if(codeIntroducedEmail == "1234" && isUserEmailCode){
             imageView.setImageResource(R.drawable.success_check)
@@ -38,6 +47,11 @@ class SignInResultValidationCode: AppCompatActivity() {
            //imageView.setBackgroundColor(getResources().getColor(R.color.errorBackground))
            imageView.setImageResource(R.drawable.checkmark_error)
            textView.text = ("El código introducido es incorrecto")
+            btnNext.setText("Volver a introducir codigo")
+            btnNext.setOnClickListener{
+                    onBackPressed()
+            }
         }
     }
+
 }
